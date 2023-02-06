9bb8f28c0148d181f3275ae9130d6673a4273274
Anastasis Marinos <80579333+E-VOL-V@users.noreply.github.com>
2023-02-06 09:57:05 +0200

    Update SoundTriggerVolume.cpp

--- SoundTriggerVolume.cpp
+++ SoundTriggerVolume.cpp
@@ -1,4 +1,4 @@
-// (C)EVOLV //
+// (C)EVOLV 2023 //
  
  
  #include "Interactables/SoundTriggerActor.h"
@@ -14,16 +14,11 @@
  	
  	// INTERACTION SYSTEM //
  	CollisionBoxScale = FVector(100.f, 100.f, 100.f);
+ 	AudioSourceLength = 0.f;
  	
  	// SOUND SYSTEM //
  	bPlayedSound = false;
  	bCanBeReplayed = false;
- 
- 	// Play sound in the actor location
- 	bPlayInPlace = true;
- 	
- 	// Play sound away from the actors location
- 	bPlayAway = false;
  	
  	// Initialize the default root
  	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(FName("DefaultSceneRoot"));
@@ -32,24 +27,30 @@
  	// Initialize the box that handles collisions
  	CollisionMesh = CreateDefaultSubobject<UBoxComponent>(FName("CollisionMesh"));
  	CollisionMesh->SetupAttachment(DefaultSceneRoot);
- 	CollisionMesh->SetBoxExtent(FVector(74.0f, 125.0f, 100.0f));
+ 	CollisionMesh->SetBoxExtent(CollisionBoxScale);
  	CollisionMesh->SetRelativeLocation(FVector(0.0f,0.0f, 0.0f));
- 	
- 	// Initialize the in place audio component
- 	AC_InPlace = CreateDefaultSubobject<UAudioComponent>(FName("AC_InPlace"));
- 	AC_InPlace->SetupAttachment(DefaultSceneRoot);
- 	AC_InPlace->SetRelativeLocation(FVector(0.0f,0.0f, 180.0f));
 
  	// Initialize spring arm
  	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
- 	SpringArm->SetupAttachment(AC_InPlace);
- 	SpringArm->TargetArmLength = 300.f;
+ 	SpringArm->SetupAttachment(DefaultSceneRoot);
+ 	SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, 180.0f));
+ 	SpringArm->TargetArmLength = AudioSourceLength;
  	
  	// Initialize the away audio component
- 	AC_Away = CreateDefaultSubobject<UAudioComponent>(FName("AC_Away"));
- 	AC_Away->SetupAttachment(SpringArm);
+ 	AudioComponent = CreateDefaultSubobject<UAudioComponent>(FName("AC_Away"));
+ 	AudioComponent->SetupAttachment(SpringArm);
  
  }
+
+// HANDLE PROPERTY CHANGES IN LEVEL DESIGNER //
+void ASoundTriggerActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+ 	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+ 	CollisionMesh->SetBoxExtent(CollisionBoxScale);
+ 	SpringArm->TargetArmLength = AudioSourceLength;
+}
+
  
  // WHEN THE GAME STARTS //
  void ASoundTriggerActor::BeginPlay()
@@ -58,13 +59,9 @@
  	//Set up the collision system
  	CollisionMesh->OnComponentBeginOverlap.AddDynamic(this,&ASoundTriggerActor::OverlapBegin);
 
- 	//Set in place sound effect
- 	AC_InPlace->SetSound(SoundEffect);
- 	AC_InPlace->Stop();
- 	
- 	//Set away sound effect
- 	AC_Away->SetSound(SoundEffect);
- 	AC_Away->Stop();
+ 	//Set sound effect for the audio component
+ 	AudioComponent->SetSound(SoundEffect);
+ 	AudioComponent->Stop();
  }
  
  // WHEN PLAYER INTERACTS WITH THE ACTOR //
@@ -74,25 +71,16 @@
  	// Trigger Sound Events
  	if(bCanBeReplayed)
  	{
- 		if(bPlayInPlace && !AC_InPlace->IsPlaying())
+ 		if(!AudioComponent->IsPlaying())
  		{
- 			AC_InPlace->Play(0.0f);
- 		}
- 		else if (bPlayAway && !AC_Away->IsPlaying())
- 		{
- 			AC_Away->Play(0.0f);
+ 			AudioComponent->Play(0.0f);
  		}
  	}
  	else if(!bPlayedSound)
  	{
- 		if(bPlayInPlace && !AC_InPlace->IsPlaying())
+ 		if(!AudioComponent->IsPlaying())
  		{
- 			AC_InPlace->Play(0.0f);
- 			bPlayedSound = true;
- 		}
- 		else if (bPlayAway && !AC_Away->IsPlaying())
- 		{
- 			AC_Away->Play(0.0f);
+ 			AudioComponent->Play(0.0f);
  			bPlayedSound = true;
  		}
  	}
